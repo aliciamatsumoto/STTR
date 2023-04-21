61728f1f813059e967ff90954749b476f01511c6
Alicia Matsumoto <aliciamatsumoto@berkeley.edu>
2023-04-20 23:15:51 -0700

    load in architecture take two

--- model_architecture.py
+++ model_architecture.py
@@ -3,6 +3,7 @@ Consolidate the model architecture definition to allow loading elsewhere.
 Author: Alicia Matsumoto
 """
 import argparse
+import torch
 import torch.nn as nn
 from models_istt import build_model
 from main_style_transfer import get_args_parser
@@ -14,3 +15,10 @@ def get_model_and_criterion():
     args = parser.parse_args()
     model, criterion, postprocessors = build_model(args)
     return model, criterion
+
+
+if __name__ == "__main__":
+    model, criterion = get_model_and_criterion()
+    checkpoint = {'model': model,
+                  'criterion': criterion}
+    torch.save(checkpoint, 'arch_to_load.pth')
