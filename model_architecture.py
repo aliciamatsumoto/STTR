b3bf941d18331f02e6a04398514ed1a932b3f5ca
Alicia Matsumoto <aliciamatsumoto@berkeley.edu>
2023-04-20 23:26:29 -0700

    take three, with demo file parse

--- model_architecture.py
+++ model_architecture.py
@@ -6,13 +6,31 @@ import argparse
 import torch
 import torch.nn as nn
 from models_istt import build_model
-from main_style_transfer import get_args_parser
+from demo_sttr_image_ACCV import get_args_parser
 
 
 def get_model_and_criterion():
     parser = argparse.ArgumentParser(
         'DETR training and evaluation script', parents=[get_args_parser()])
-    args = parser.parse_args()
+    args = parser.parse_args([
+                          "--batch_size","1",
+                          "--style_loss_coef","10",
+                          "--fold_k","5",
+                          "--lr_backbone","1e-5",
+                          "--lr","1e-5",
+                          "--enc_layers","6",
+                          "--dec_layers","6",
+                          "--model_type","nofold",
+                          "--enorm","--dnorm","--tnorm",
+                          "--cbackbone_layer","2","--sbackbone_layer","4",
+                          
+                          "--dataset_file","demo",
+                          "--resume","checkpoint_model/checkpoint0005.pth"  ,
+                          "--img_size","512",
+                          "--in_content_folder","inputs/content",
+                          "--style_folder","inputs/style",
+                          "--output_dir","outputs",
+                             ])
     model, criterion, postprocessors = build_model(args)
     return model, criterion
 
